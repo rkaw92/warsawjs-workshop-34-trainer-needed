1a0ba18ceb5d6a3162188ffd0ec5bb0f0565edcf
Robert Kawecki <robert+github@rkaw.pl>
2019-07-20 00:56:52 +0200

    Add a front-end index and a server

---
+++ server.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const HTTP_PORT = process.env.HTTP_PORT || 3000;
+
+const express = require('express');
+const app = express();
+const path = require('path');
+const colors = require('colors');
+
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+app.listen(HTTP_PORT, function() {
+  console.log('Server ready: ' + colors.green('http://localhost:%d'), HTTP_PORT);
+});
