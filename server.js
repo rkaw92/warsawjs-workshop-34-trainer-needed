6fe10ef9782c076b6848fa37ab03f104b9bfd8ec
Robert Kawecki <robert+github@rkaw.pl>
2019-07-24 22:27:35 +0200

    Add a dummy WebSocket server

--- server.js
+++ server.js
@@ -3,13 +3,31 @@
 const HTTP_PORT = process.env.HTTP_PORT || 3000;
 
 const express = require('express');
-const app = express();
+const http = require('http');
 const path = require('path');
 const colors = require('colors');
+const WebSocket = require('ws');
+
+const app = express();
+const server = http.createServer(app);
 
 app.use(express.static(path.resolve(__dirname, 'public')));
 app.use('/assets', express.static(path.resolve(__dirname, 'dist')));
 
-app.listen(HTTP_PORT, function() {
+const wss = new WebSocket.Server({
+  server: server
+});
+wss.on('connection', function(connection) {
+  connection.on('message', function(message) {
+    try {
+      console.log(message);
+      connection.send(message);
+    } catch (error) {
+      console.error(error);
+    }
+  });
+});
+
+server.listen(HTTP_PORT, function() {
   console.log('Server ready: ' + colors.green('http://localhost:%d'), HTTP_PORT);
 });
